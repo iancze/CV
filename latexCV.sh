60f2e682e513052b62ac7668a0256475421a7dc7
Ian Czekala <iancze@gmail.com>
2019-10-15 20:43:24 -0700

    added JB's new paper.

--- latexCV.sh
+++ latexCV.sh
@@ -1,53 +1,8 @@
 #!/bin/sh
-# var=mypubs # The processed library
+# var=mypubs # The procesgsed library
 # var2=mylibrary # The raw library downloaded from ADS
 
 # clear old intermediate files
-rm *.aux
-rm *.bbl
-rm *.blg
-rm *.log
-
-# fix Latex errors
-sed -i -re 's/(\{\\prime\})/\$\^\\prime\$/g' first_papers.bib
-sed -i -re 's/(\{\\prime\})/\$\^\\prime\$/g' all_papers.bib
-sed -i -re 's/(\{\\prime\})/\$\^\\prime\$/g' preprint_papers.bib
-sed -i -re 's/(\{\\ap\})/\$\\approx\$/g' first_papers.bib
-sed -i -re 's/(\{\\ap\})/\$\\approx\$/g' all_papers.bib
-sed -i -re 's/(\{\\ap\})/\$\\approx\$/g' preprint_papers.bib
-
-pdflatex main.tex
-bibtex first
-bibtex all
-bibtex preprint
-
-# Leave all the formatting alone for now
-# Remove line breaks so that sed can always find my name
-sed -i -re '/\{Czekala\},$/{N;s/\n//;}' first.bbl
-sed -i -re '/\{Czekala\},$/{N;s/\n//;}' all.bbl
-sed -i -re '/\{Czekala\},$/{N;s/\n//;}' preprint.bbl
-
-# Always use my full name
-# make my name bold
-sed -i -re 's/(\{Czekala\},[ ]{1,}I.)/\{Czekala\}, Ian/g' first.bbl
-sed -i -re 's/(\{Czekala\},[ ]{1,}I.;)/\{Czekala\}, Ian;/g' first.bbl
-sed -i -re 's/(\{Czekala\},[ ]{1,}Ian)/\\textbf\{\1\}/g' first.bbl
-sed -i -re 's/(\{Czekala\},[ ]{1,}Ian;)/\\textbf\{\1\}/g' first.bbl
-
-sed -i -re 's/(\{Czekala\},[ ]{1,}I.)/\{Czekala\}, Ian/g' all.bbl
-sed -i -re 's/(\{Czekala\},[ ]{1,}I.;)/\{Czekala\}, Ian;/g' all.bbl
-sed -i -re 's/(\{Czekala\},[ ]{1,}Ian)/\\textbf\{\1\}/g' all.bbl
-sed -i -re 's/(\{Czekala\},[ ]{1,}Ian;)/\\textbf\{\1\}/g' all.bbl
-
-sed -i -re 's/(\{Czekala\},[ ]{1,}I.)/\{Czekala\}, Ian/g' preprint.bbl
-sed -i -re 's/(\{Czekala\},[ ]{1,}I.;)/\{Czekala\}, Ian;/g' preprint.bbl
-sed -i -re 's/(\{Czekala\},[ ]{1,}Ian)/\\textbf\{\1\}/g' preprint.bbl
-sed -i -re 's/(\{Czekala\},[ ]{1,}Ian;)/\\textbf\{\1\}/g' preprint.bbl
-
-pdflatex main.tex
-pdflatex main.tex
-
-# clear intermediate files again
 rm *.aux
 rm *.bbl
 rm *.blg
@@ -56,3 +11,46 @@ rm *.fdb_latexmk
 rm *.fls
 rm *.log
 rm *.out
+
+# fix Latex errors
+gsed -i -re 's/(\{\\prime\})/\$\^\\prime\$/g' first_papers.bib
+gsed -i -re 's/(\{\\prime\})/\$\^\\prime\$/g' all_papers.bib
+gsed -i -re 's/(\{\\prime\})/\$\^\\prime\$/g' preprint_papers.bib
+gsed -i -re 's/(\{\\ap\})/\$\\approx\$/g' first_papers.bib
+gsed -i -re 's/(\{\\ap\})/\$\\approx\$/g' all_papers.bib
+gsed -i -re 's/(\{\\ap\})/\$\\approx\$/g' preprint_papers.bib
+
+pdflatex main.tex
+bibtex first
+bibtex all
+bibtex preprint
+
+# Leave all the formatting alone for now
+# Remove line breaks so that gsed can always find my name
+gsed -i -re '/\{Czekala\},$/{N;s/\n//;}' first.bbl
+gsed -i -re '/\{Czekala\},$/{N;s/\n//;}' all.bbl
+gsed -i -re '/\{Czekala\},$/{N;s/\n//;}' preprint.bbl
+
+# Always use my full name
+# make my name bold
+gsed -i -re 's/(\{Czekala\},[ ]{1,}I.)/\{Czekala\}, Ian/g' first.bbl
+gsed -i -re 's/(\{Czekala\},[ ]{1,}I.;)/\{Czekala\}, Ian;/g' first.bbl
+gsed -i -re 's/(\{Czekala\},[ ]{1,}Ian)/\\textbf\{\1\}/g' first.bbl
+gsed -i -re 's/(\{Czekala\},[ ]{1,}Ian;)/\\textbf\{\1\}/g' first.bbl
+
+gsed -i -re 's/(\{Czekala\},[ ]{1,}I.)/\{Czekala\}, Ian/g' all.bbl
+gsed -i -re 's/(\{Czekala\},[ ]{1,}I.;)/\{Czekala\}, Ian;/g' all.bbl
+gsed -i -re 's/(\{Czekala\},[ ]{1,}Ian)/\\textbf\{\1\}/g' all.bbl
+gsed -i -re 's/(\{Czekala\},[ ]{1,}Ian;)/\\textbf\{\1\}/g' all.bbl
+
+gsed -i -re 's/(\{Czekala\},[ ]{1,}I.)/\{Czekala\}, Ian/g' preprint.bbl
+gsed -i -re 's/(\{Czekala\},[ ]{1,}I.;)/\{Czekala\}, Ian;/g' preprint.bbl
+gsed -i -re 's/(\{Czekala\},[ ]{1,}Ian)/\\textbf\{\1\}/g' preprint.bbl
+gsed -i -re 's/(\{Czekala\},[ ]{1,}Ian;)/\\textbf\{\1\}/g' preprint.bbl
+
+pdflatex main.tex
+pdflatex main.tex
+
+
+# update version on website
+cp main.pdf ~/Documents/Professional/iancze.github.io/assets/Ian_CzekalaCV.pdf
